f9787d74b843473d1a136b00d1631444d8c8c9f5
alan910127 <alan469357@gmail.com>
2022-09-30 23:25:35 +0800

    feat: implemented `clampedExpVector`

--- HW1/part1/vectorOP.cpp
+++ HW1/part1/vectorOP.cpp
@@ -1,8 +1,7 @@
 #include "PPintrin.h"
 
 // implementation of absSerial(), but it is vectorized using PP intrinsics
-void absVector(float *values, float *output, int N)
-{
+void absVector(float* values, float* output, int N) {
   __pp_vec_float x;
   __pp_vec_float result;
   __pp_vec_float zero = _pp_vset_float(0.f);
@@ -11,8 +10,7 @@ void absVector(float *values, float *output, int N)
   //  Note: Take a careful look at this loop indexing.  This example
   //  code is not guaranteed to work when (N % VECTOR_WIDTH) != 0.
   //  Why is that the case?
-  for (int i = 0; i < N; i += VECTOR_WIDTH)
-  {
+  for (int i = 0; i < N; i += VECTOR_WIDTH) {
 
     // All ones
     maskAll = _pp_init_ones();
@@ -40,8 +38,9 @@ void absVector(float *values, float *output, int N)
   }
 }
 
-void clampedExpVector(float *values, int *exponents, float *output, int N)
-{
+#define MAX_VALUE 9.999999f
+
+void clampedExpVector(float* values, int* exponents, float* output, int N) {
   //
   // PP STUDENTS TODO: Implement your vectorized version of
   // clampedExpSerial() here.
@@ -49,23 +48,62 @@ void clampedExpVector(float *values, int *exponents, float *output, int N)
   // Your solution should work for any value of
   // N and VECTOR_WIDTH, not just when VECTOR_WIDTH divides N
   //
-  for (int i = 0; i < N; i += VECTOR_WIDTH)
-  {
+
+  __pp_vec_float bases, result;
+  __pp_vec_float maxValue = _pp_vset_float(MAX_VALUE);
+  __pp_vec_int exps, currentExps;
+  __pp_vec_int ones = _pp_vset_int(1);
+  __pp_mask maskIsValidPosition, maskNeedMultiply, maskNeedClamp;
+
+  for (int i = 0; i < N; i += VECTOR_WIDTH) {
+    // create a mask of valid positions
+    size_t numValidPositions = min(N - i, VECTOR_WIDTH);
+    maskIsValidPosition = _pp_init_ones(numValidPositions);
+
+    // load valid data
+    _pp_vload_float(bases, values + i, maskIsValidPosition);
+    _pp_vload_int(exps, exponents + i, maskIsValidPosition);
+    _pp_vset_float(result, 1.0f, maskIsValidPosition);
+
+    // set currentExp to 0
+    _pp_vset_int(currentExps, 0, maskIsValidPosition);
+
+    // needMultiply = isValidPosition && (currentExp < exp);
+    maskNeedMultiply = _pp_init_ones(0);
+    _pp_vlt_int(maskNeedMultiply, currentExps, exps, maskIsValidPosition);
+
+    while (_pp_cntbits(maskNeedMultiply) > 0) {
+      // result *= base;
+      _pp_vmult_float(result, result, bases, maskNeedMultiply);
+
+      // currentExp++;
+      _pp_vadd_int(currentExps, currentExps, ones, maskNeedMultiply);
+
+      // recalculate needMultiply
+      maskNeedMultiply = _pp_init_ones(0);
+      _pp_vlt_int(maskNeedMultiply, currentExps, exps, maskIsValidPosition);
+    }
+
+    // clamp at MAX_VALUE
+    maskNeedClamp = _pp_init_ones(0);
+    _pp_vgt_float(maskNeedClamp, result, maxValue, maskIsValidPosition);
+    _pp_vset_float(result, MAX_VALUE, maskNeedClamp);
+
+    // store back computed results
+    _pp_vstore_float(output + i, result, maskIsValidPosition);
   }
 }
 
 // returns the sum of all elements in values
 // You can assume N is a multiple of VECTOR_WIDTH
 // You can assume VECTOR_WIDTH is a power of 2
-float arraySumVector(float *values, int N)
-{
+float arraySumVector(float* values, int N) {
 
   //
   // PP STUDENTS TODO: Implement your vectorized version of arraySumSerial here
   //
 
-  for (int i = 0; i < N; i += VECTOR_WIDTH)
-  {
+  for (int i = 0; i < N; i += VECTOR_WIDTH) {
   }
 
   return 0.0;
