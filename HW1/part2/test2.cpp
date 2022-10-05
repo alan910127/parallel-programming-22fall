e552003c60572b646c790976694cd690fa809c47
alan910127 <alan469357@gmail.com>
2022-10-05 10:19:26 +0800

    style: auto formatting

--- HW1/part2/test2.cpp
+++ HW1/part2/test2.cpp
@@ -2,17 +2,14 @@
 #include "test.h"
 #include "fasttime.h"
 
-void test2(float *__restrict a, float *__restrict b, float *__restrict c, int N)
-{
+void test2(float* __restrict a, float* __restrict b, float* __restrict c, int N) {
   __builtin_assume(N == 1024);
-  a = (float *)__builtin_assume_aligned(a, 16);
-  b = (float *)__builtin_assume_aligned(b, 16);
+  a = (float*)__builtin_assume_aligned(a, 16);
+  b = (float*)__builtin_assume_aligned(b, 16);
 
   fasttime_t time1 = gettime();
-  for (int i = 0; i < I; i++)
-  {
-    for (int j = 0; j < N; j++)
-    {
+  for (int i = 0; i < I; i++) {
+    for (int j = 0; j < N; j++) {
       /* max() */
       c[j] = a[j];
       if (b[j] > a[j])
@@ -24,5 +21,5 @@ void test2(float *__restrict a, float *__restrict b, float *__restrict c, int N)
 
   double elapsedf = tdiff(time1, time2);
   std::cout << "Elapsed execution time of the loop in test2():\n"
-            << elapsedf << "sec (N: " << N << ", I: " << I << ")\n";
+    << elapsedf << "sec (N: " << N << ", I: " << I << ")\n";
 }
