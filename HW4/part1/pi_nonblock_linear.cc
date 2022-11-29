0896a587938dcf2ef89d603a783a3d9981f0c116
alan910127 <alan469357@gmail.com>
2022-11-30 03:18:06 +0800

    :sparkles: Nonblocking linear PI

--- HW4/part1/pi_nonblock_linear.cc
+++ HW4/part1/pi_nonblock_linear.cc
@@ -5,7 +5,21 @@
 #include <time.h>
 #include <unistd.h>
 
-int main(int argc, char **argv) {
+#include <vector>
+
+#define MASTER 0
+
+using rand_state_t = long;
+
+int fast_rand(rand_state_t* state) {
+  return (((*state = *state * 214013L + 2531011L) >> 16) & 0x7fff);
+}
+
+double get_random(rand_state_t* state) {
+  return (double)fast_rand(state) / 0x7fff;
+}
+
+int main(int argc, char** argv) {
   // --- DON'T TOUCH ---
   MPI_Init(&argc, &argv);
   double start_time = MPI_Wtime();
@@ -14,20 +28,54 @@ int main(int argc, char **argv) {
   int world_rank, world_size;
   // ---
 
-  // TODO: MPI init
+  // MPI init
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-  if (world_rank > 0) {
-    // TODO: MPI workers
-  } else if (world_rank == 0) {
-    // TODO: non-blocking MPI communication.
-    // Use MPI_Irecv, MPI_Wait or MPI_Waitall.
-    MPI_Request requests[];
+  long long int num_tosses =
+      tosses / world_size + (world_rank < tosses % world_size ? 1 : 0);
 
-    MPI_Waitall();
+  rand_state_t state = 0xaf * world_rank;
+  long long int pi_count = 0;
+
+  for (long long int i = 0; i < num_tosses; ++i) {
+    double x = get_random(&state);
+    double y = get_random(&state);
+    if (x * x + y * y <= 1.0) {
+      ++pi_count;
+    }
   }
 
-  if (world_rank == 0) {
-    // TODO: PI result
+  int tag = 0;
+  if (world_rank > MASTER) {
+    // MPI workers
+    MPI_Request request;
+    MPI_Isend(&pi_count, 1, MPI_LONG_LONG_INT, MASTER, tag, MPI_COMM_WORLD,
+              &request);
+    MPI_Request_free(&request);  // No need to perform wait
+  } else if (world_rank == MASTER) {
+    // non-blocking MPI communication.
+    // Use MPI_Irecv, MPI_Wait or MPI_Waitall.
+    int num_requests = world_size - 1;
+    std::vector<MPI_Request> requests(num_requests);
+    std::vector<MPI_Status> status(num_requests);
+    std::vector<long long int> peer_count(num_requests);
+
+    for (int peer = 1; peer < world_size; ++peer) {
+      MPI_Irecv(&peer_count[peer - 1], 1, MPI_LONG_LONG_INT, peer, tag,
+                MPI_COMM_WORLD, &requests[peer - 1]);
+    }
+
+    MPI_Waitall(num_requests, requests.data(), status.data());
+
+    for (auto& count : peer_count) {
+      pi_count += count;
+    }
+  }
+
+  if (world_rank == MASTER) {
+    // PI result
+    pi_result = 4.0 * pi_count / tosses;
 
     // --- DON'T TOUCH ---
     double end_time = MPI_Wtime();
