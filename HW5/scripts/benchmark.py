fc8916669cefdc657f622140ed2aae5f90243178
alan910127 <alan469357@gmail.com>
2022-12-24 01:15:03 +0800

    :hammer: Add command line arguments for different cases

--- HW5/scripts/benchmark.py
+++ HW5/scripts/benchmark.py
@@ -1,38 +1,46 @@
 import re
 import statistics
 import subprocess
+import sys
 from pathlib import Path
+from typing import Sequence
 
 RUNNER = Path.home() / "HW5" / "scripts" / "run.sh"
 TIME_PATTERN = re.compile(r"\[mandelbrot thread\].*\[(\d+\.\d+)\] ms")
-MAX_ITERATION = 10
+RUN_COUNT = 10
+VIEW = 1
+MAX_ITERATION = 256
 
 
 def get_kernels():
-    for i in range(1, 5):
+    for i in range(1, 4):
         yield f"kernel{i}.cu"
 
 
-def run_task(version: int) -> float:
+def run_task(version: int, view: str = "1", iterations: str = "256") -> float:
     matched = None
 
     while matched is None:
         result: subprocess.CompletedProcess[str] = subprocess.run(
-            ["sh", RUNNER, str(version)], stdout=subprocess.PIPE, encoding="utf-8"
+            ["sh", RUNNER, str(version), view, iterations],
+            stdout=subprocess.PIPE,
+            encoding="utf-8",
         )
         matched = TIME_PATTERN.search(result.stdout)
 
     return float(matched.group(1))
 
 
-def main() -> None:
+def main(args: Sequence[str]) -> None:
+    _, view, iterations, *_ = args
+    print(f"{view=}, {iterations=}")
     for k in range(1, 5):
         times: list[float] = []
-        for iteration in range(1, MAX_ITERATION + 1):
-            print(f"\rRunning 'kernel{k}.cu'... {iteration}/{MAX_ITERATION}", end="")
+        for iteration in range(1, RUN_COUNT + 1):
+            print(f"\rRunning 'kernel{k}.cu'... {iteration}/{RUN_COUNT}", end="")
             if times:
                 print(f" estimated: {statistics.mean(times):.3f} ms", end="")
-            elapsed = run_task(k)
+            elapsed = run_task(k, view, iterations)
             times.append(elapsed)
         print(
             f" (λ ± σ){statistics.mean(times):.3f} ± {statistics.stdev(times):.3f} ms"
@@ -40,4 +48,4 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
--- HW5/scripts/run.sh
+++ HW5/scripts/run.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 VERSION="${1:-1}"
+VIEW="$2"
+MAX_ITERATION="$3"
 
 cd /home/109652039/HW5/src
 
 cp "kernel${VERSION}.cu" kernel.cu
 make
-./mandelbrot
+./mandelbrot --gpu-only 1 --view "${VIEW}" --iter "${MAX_ITERATION}"
 
 cd -
